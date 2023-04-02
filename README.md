49c769588354275b5154692ddfddacbc33353c70
Vamship113 <vinnup113@gmail,com>
2023-04-02 18:22:33 +0530

    uploaded flutter project dealing with localization

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Flutter-Localisation
+# flutter_localization
+
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localization_v2/router/custom_router.dart';
+import 'package:flutter_localization_v2/router/route_constants.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static void setLocale(BuildContext context,Locale newLocale){
+    _MyAppState? state=context.findAncestorStateOfType<_MyAppState>();
+    state?.setLocale(newLocale);
+  }
+
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale?_locale;
+  setLocale(Locale locale){
+    setState(() {
+      _locale=locale;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Localization',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: _locale,
+      onGenerateRoute: CustomRouter.generatedRoute,
+      initialRoute: homeRoute,
+    );
+  }
+}
